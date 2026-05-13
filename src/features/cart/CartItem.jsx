c6afba1a32eace2badf58bd49f95f4632167b4a5
Daria <kordesovad@gmail.com>
2026-05-13 11:18:37 +0200

    now prices are correct in the cart

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,72 @@
+import { pizzaDeleted, pizzaDecrease } from './cartSlice';
+import { usePizzaIncrease } from './usePizzaIncrease';
+import { useDispatch } from 'react-redux';
+
+export const CartItem = ({ pizza }) => {
+  const dispatch = useDispatch();
+  const deletePizza = (id) => {
+    dispatch(pizzaDeleted(id)); //pizzaId
+  };
+  const decreasePizza = (id) => {
+    dispatch(pizzaDecrease(id));
+  };
+  const increasePizza = usePizzaIncrease();
+  const totalPrice = pizza.options.reduce((sum, item) => sum + item.price, 0);
+
+  return (
+    <li key={pizza.id}>
+      <article className="cart-item">
+        <img
+          className="cart-item__image"
+          src={pizza.imageUrl}
+          alt={pizza.name}
+        />
+        <div className="cart-item__info">
+          <span className="cart-item__badge">{pizza.category}</span>
+          <h3 className="cart-item__name">{pizza.name}</h3>
+          <p className="cart-item__description">{pizza.description}</p>
+        </div>
+        <div className="cart-item__divider" />
+        <div className="cart-item__details">
+          {pizza.options.map((option) => (
+            <div key={option.size} className="cart-item__option">
+              <div className="cart-item__option-info">
+                <span className="cart-item__details-size">{option.size}</span>
+                <span className="cart-item__details-dough">Без изменений</span>
+                <span className="cart-item__details-mods">Без изменений</span>
+              </div>
+              <span className="cart-item__option-price">${option.price}</span>
+            </div>
+          ))}
+        </div>
+        <div className="cart-item__divider" />
+        <div className="cart-item__qty">
+          <button
+            type="button"
+            onClick={() => decreasePizza(pizza.pizzaId)}
+            className="cart-item__qty-btn"
+          >
+            −
+          </button>
+          <span className="cart-item__qty-count">{pizza.amount}</span>
+          <button
+            type="button"
+            onClick={() => increasePizza(pizza)}
+            className="cart-item__qty-btn"
+          >
+            +
+          </button>
+        </div>
+        <div className="cart-item__divider" />
+        <span className="cart-item__total">${totalPrice}</span>
+        <button
+          type="button"
+          onClick={() => deletePizza(pizza.pizzaId)}
+          className="cart-item__delete"
+        >
+          🗑
+        </button>
+      </article>
+    </li>
+  );
+};
--- src/features/cart/CartList.jsx
+++ src/features/cart/CartList.jsx
@@ -1,88 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './CartList.css';
-import { pizzaDeleted, pizzaDecrease, cartCleared } from './cartSlice';
-import { usePizzaIncrease } from './usePizzaIncrease';
+import { cartCleared } from './cartSlice';
+import { CartItem } from './CartItem';
 export const CartList = () => {
   const pizzas = useSelector((state) => state.cart);
-  console.log(pizzas); //
   const dispatch = useDispatch();
-  const deletePizza = (id) => {
-    dispatch(pizzaDeleted(id)); //pizzaId
-  };
-  const decreasePizza = (id) => {
-    dispatch(pizzaDecrease(id));
-  };
+
   const clearCart = () => {
     dispatch(cartCleared());
   };
-  const increasePizza = usePizzaIncrease();
 
   return (
     <section>
       <h2>Содержимое корзины</h2>
       <ul className="cart-list">
         {pizzas.map((pizza) => (
-          <li key={pizza.id}>
-            <article className="cart-item">
-              <img
-                className="cart-item__image"
-                src={pizza.imageUrl}
-                alt={pizza.name}
-              />
-              <div className="cart-item__info">
-                <span className="cart-item__badge">{pizza.category}</span>
-                <h3 className="cart-item__name">{pizza.name}</h3>
-                <p className="cart-item__description">{pizza.description}</p>
-              </div>
-              <div className="cart-item__divider" />
-              <div className="cart-item__details">
-                {pizza.options.map((option) => (
-                  <div key={option.size} className="cart-item__option">
-                    <span className="cart-item__details-size">
-                      {option.size}
-                    </span>
-                    <span className="cart-item__details-dough">
-                      Без изменений
-                    </span>
-                    <span className="cart-item__details-mods">
-                      Без изменений
-                    </span>
-                  </div>
-                ))}
-              </div>
-              <div className="cart-item__divider" />
-              <span className="cart-item__price">${pizza.price}</span>
-              <div className="cart-item__divider" />
-              <div className="cart-item__qty">
-                <button
-                  type="button"
-                  onClick={() => decreasePizza(pizza.pizzaId)}
-                  className="cart-item__qty-btn"
-                >
-                  −
-                </button>
-                <span className="cart-item__qty-count">{pizza.amount}</span>
-                <button
-                  type="button"
-                  onClick={() => increasePizza(pizza)}
-                  className="cart-item__qty-btn"
-                >
-                  +
-                </button>
-              </div>
-              <div className="cart-item__divider" />
-              <span className="cart-item__total">
-                ${(pizza.price * pizza.amount).toFixed(2)}
-              </span>
-              <button
-                type="button"
-                onClick={() => deletePizza(pizza.pizzaId)}
-                className="cart-item__delete"
-              >
-                🗑
-              </button>
-            </article>
-          </li>
+          <CartItem pizza={pizza} />
         ))}
       </ul>
 
